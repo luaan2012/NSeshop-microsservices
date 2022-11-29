4e4d01212236efc5dc1e1700422cc5749a05c999
luan <oempreg.yes@outlook.com.br>
2022-11-29 17:50:16 -0300

    new upd

--- src/web/WebMVC/Controllers/HomeController.cs
+++ src/web/WebMVC/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebMVC.Controllers
             return View();
         }
 
+        public IActionResult Loja()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
